371e20ad8819b44d3ef488dcc8c08beb534d9987
Magnus <magnus@bonusfamilien.dk>
2024-04-12 12:53:22 +0200

    event mesh dependency added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    kotlin("jvm") version "1.9.23"
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
+
