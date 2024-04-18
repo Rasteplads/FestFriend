a500a95116389d567d59ab6383ead0530f309b51
laurits-mumberg <laurits.mumberg@gmail.com>
2024-04-18 09:46:24 +0200

    Cleanup

--- app/src/main/java/com/rasteplads/festfriend/pages/MapPage.kt
+++ app/src/main/java/com/rasteplads/festfriend/pages/MapPage.kt
@@ -1,10 +1,8 @@
 package com.rasteplads.festfriend.pages
 
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,34 +11,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.materialIcon
-import androidx.compose.material.icons.sharp.MailOutline
 import androidx.compose.material.icons.sharp.Send
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -58,12 +44,8 @@ import com.rasteplads.festfriend.R
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
-import org.osmdroid.views.overlay.ItemizedIconOverlay
-import org.osmdroid.views.overlay.ItemizedOverlayWithFocus
 import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.OverlayItem
 import com.rasteplads.festfriend.Friends
-import com.rasteplads.festfriend.Position
 import org.osmdroid.util.BoundingBox
 
 @Composable
@@ -89,8 +71,9 @@ fun GroupIdDisplay(groupID: String) {
         val copyToast = Toast.makeText(LocalContext.current, "Copied to clipboard", Toast.LENGTH_SHORT)
         Box(
             modifier = Modifier
-                .clip(shape = RoundedCornerShape(16.dp)) // Apply rounded corners with 16dp radius
-                .background(Color.White) // Set background color for better visibility
+                .clip(shape = RoundedCornerShape(16.dp))
+                .background(Color.White)
+                // Copy the code to clipboard when clicking
                 .clickable(onClick = {
                     clipboardManager.setText(AnnotatedString(groupID))
                     copyToast.show()
@@ -121,10 +104,9 @@ fun rememberMapViewWithLifecycle(friends: Friends): MapView {
         }
     }
 
+    // Changes map bounding box to friends
     var hasZoomed by remember { mutableStateOf(false) }
-
     LaunchedEffect(friends) {
-        //Overlay
         if (friends.size >= 1 && !hasZoomed) {
             mapView.doOnLayout {
                 zoomToFriends(friends, mapView)
@@ -133,14 +115,10 @@ fun rememberMapViewWithLifecycle(friends: Friends): MapView {
         }
     }
 
-
-
     // Updates markers when there are changes to friends
     LaunchedEffect(friends) {
         createMarkers(friends, mapView)
     }
-
-
 
     // Makes MapView follow the lifecycle of this composable (nej jeg ved ikke hvad det betyder)
     val lifecycleObserver = rememberMapLifecycleObserver(mapView)
@@ -151,7 +129,7 @@ fun rememberMapViewWithLifecycle(friends: Friends): MapView {
             lifecycle.removeObserver(lifecycleObserver)
         }
     }
-    Configuration.getInstance().setUserAgentValue("github-laurits-mumberg-myapp");
+    Configuration.getInstance().setUserAgentValue("github-rasteplads-festFriendApp");
     mapView.setTileSource(TileSourceFactory.MAPNIK)
 
     return mapView
@@ -174,8 +152,6 @@ fun zoomToFriends(friends: Friends, map: MapView){
 
     map.zoomToBoundingBox(BoundingBox(latMax.toDouble() + zoomPadding,lngMax.toDouble() + zoomPadding,latMin.toDouble() - zoomPadding,lngMin.toDouble() - zoomPadding),false,1,
         1000.0,500)
-    Log.d("Testing", "Hello: " + map.boundingBox.toString())
-
 }
 
 fun createMarkers (friends: Friends, map: MapView){
