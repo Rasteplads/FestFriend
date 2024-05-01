e2c0d3bfb30b8b9487dc5f2fb663ae4ac3a96a3e
laurits-mumberg <laurits.mumberg@gmail.com>
2024-05-01 08:51:09 +0200

    new button

--- app/src/main/java/com/rasteplads/festfriend/pages/shared/BackButton.kt
+++ app/src/main/java/com/rasteplads/festfriend/pages/shared/BackButton.kt
@@ -3,10 +3,16 @@ package com.rasteplads.festfriend.pages.shared
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.ArrowBack
+import androidx.compose.material.icons.sharp.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 
@@ -17,10 +23,7 @@ fun BackButton(onClick: () -> Unit){
         modifier = Modifier.fillMaxWidth(1f)
     ){
         TextButton(onClick = onClick) {
-            Text(
-                text = "👈😎",
-                style = TextStyle(fontSize = 24.sp)
-            )
+            Icon(imageVector = Icons.Sharp.ArrowBack, contentDescription = "Copy", tint = MaterialTheme.colorScheme.primary)
         }
     }
 }
